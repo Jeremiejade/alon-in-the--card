14ee6354efffa9cac051772f97865c04a3bf8ca8
Jadé Jérémie <jade.jeremie@gmail.com>
2022-01-23 16:11:48 +0100

    Improve shuffle cards

--- js/utils.js
+++ js/utils.js
@@ -13,8 +13,16 @@ export function createCards() {
   return cards;
 }
 
-export function shuffleArray(array, random = Math.random) {
-  return [...array].sort(() => 0.5 - random());
+export function shuffleArray(array) {
+  const newArray = [...array];
+
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = newArray[i];
+    newArray[i] = newArray[j];
+    newArray[j] = temp;
+  }
+  return newArray;
 }
 
 export function getColorSide(color) {
